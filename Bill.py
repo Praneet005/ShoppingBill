cfd5f2cc8ff584dc8d150d6100522444fff84bcf
Praneet Kamalakannan <121420706+Praneet005@users.noreply.github.com>
2023-11-17 01:03:28 +0530

    Add files via upload

---
+++ Bill.py
@@ -0,0 +1,36 @@
+def calculate_bill(items):
+    total = 0
+
+    print("\n*** Shopping Bill ***\n")
+    print("{:<15} {:<10} {:<10} {:<10}".format("Item", "Price", "Quantity", "Total"))
+
+    for item, details in items.items():
+        price = details['price']
+        quantity = details['quantity']
+        item_total = price * quantity
+        total += item_total
+
+        print("{:<15} ${:<10.2f} {:<10} ${:<10.2f}".format(item, price, quantity, item_total))
+
+    print("\nTotal Bill: ${:.2f}\n".format(total))
+
+
+def main():
+    items = {}
+
+    while True:
+        item_name = input("Enter item name (or 'done' to finish): ").strip().lower()
+
+        if item_name == 'done':
+            break
+
+        price = float(input("Enter the price per unit: "))
+        quantity = int(input("Enter the quantity: "))
+
+        items[item_name] = {'price': price, 'quantity': quantity}
+
+    calculate_bill(items)
+
+
+if __name__ == "__main__":
+    main()
